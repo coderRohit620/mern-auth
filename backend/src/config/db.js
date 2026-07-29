2ed1851d087b10b450a8832a73dc5e349bd6afab
coderRohit620 <rohitravi62023@gmail.com>
2026-07-29 16:46:03 +0530

    Initial commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connetionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}`
+        );
+        console.log(
+            `\n MongoDB connected !! DB HOST: ${connetionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
