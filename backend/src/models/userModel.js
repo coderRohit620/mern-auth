3c74315f3cd84fd912ff73b48a07e19a02bd26e9
coderRohit620 <rohitravi62023@gmail.com>
2026-07-29 21:40:15 +0530

    feat: add user model with schema, password hashing, and token generation methods

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+
+        username:{
+            type:String,
+            required: true,
+            unique:true,
+            lowercase:true,
+        },
+        email:{
+            type:String,
+            required: true,
+            unique: true,
+            lowercase:true,
+        },
+
+        password:{
+            type:String,
+            required:[true , 'Password is Required'],
+        },
+
+        refreshToken:{
+            type:String
+        }
+
+    },
+    {
+        timestamps:true
+    }
+);
+
+userSchema.pre("save", async function() {
+    if(!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this._email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRATION
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRATION
+        }
+    )
+}
+
+
+export const User = mongoose.model("User" ,userSchema)
